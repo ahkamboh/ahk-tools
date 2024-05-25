842ddba5e32b0e1e799689b6904bacc8fafaac0a
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2024-05-25 12:44:50 +0500

    push

---
+++ app/controllers/cv_controller.rb
@@ -0,0 +1,10 @@
+class   CvController < ApplicationController
+
+ def temp1
+    @condition_for_first_page = true
+  end
+
+  def temp2
+    @condition_for_second_page = true
+  end
+end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+    # Your controller actions here
+
+end
+  
